03e9a65b86bd66418afcbb6c4a182be3ca24ab1c
K0stka <www.honza.90@gmail.com>
2025-02-28 15:38:25 +0100

    Updated next.config.ts for deprecation reasons

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	devIndicators: {
-		appIsrStatus: false,
-	},
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
